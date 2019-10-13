c34b1b0b81cdd15e5f32a83813a9d118be68a1b8
Zachary Shorts <shorts23@gmail.com>
2019-10-12 23:28:37 -0500

    added title

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,7 +3,7 @@ import {Router} from '@angular/router';
 import {auth} from 'firebase/app';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {AngularFirestore, AngularFirestoreDocument} from '@angular/fire/firestore';
-import {User} from './user.model';
+import {User} from '../models/user.model';
 import {Observable, of} from 'rxjs';
 import {switchMap} from 'rxjs/operators';
 
